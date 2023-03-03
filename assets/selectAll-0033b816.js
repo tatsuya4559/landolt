ffff2852b7233d298274d927204975237a7914da
tatsuya4559 <tatsuya.k1029@gmail.com>
2023-03-03 21:45:38 +0900

    build

---
+++ assets/selectAll-0033b816.js
@@ -0,0 +1,1 @@
+import{W as t,X as n,Y as o}from"./index-632d48c9.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
